ac77e943a3d697824e5644ea4428daa22265cf06
Tetsuya Ueda <maybe.t.ueda@gmail.com>
2019-07-02 22:25:01 +0900

    initial commit

---
+++ script/checkParameter.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# check number of args
+if [$# -eq 0]:then
+  echo "number of args=$# or arg is blank" 1>&2
+  exit 1
+fi
+
+if [$# -gt 1 ]:then
+  echo "number of args=$#" 1>&2
+  echo "Try with 1 arg" 1>&2
+  exit 2
+fi
+
+# check none
+if [$1 -eq 'none']:then
+  echo "arg is none" 1>&2
+  exit 3
+fi
+
+exit /b 0
---
+++ script/mount.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+@echo off
+
+# number of args
+args=4
+
+# check number of args
+if [$# -ne $args]:then
+  echo "number of args=$#" 1>&2
+  echo "number of args should be $args" 1>&2
+  exit 1
+fi
+
+# set parameters
+ipaddr=$1
+serverDir=$2
+cliantDir=$3
+password=$4
+
+# mount
+mount -t cifs //$ipaddr/$serverDir $cliantDir -o pass=$password
+
+# mount check
+mountpoint -q $cliantDir
+if [$?=1]:then
+  echo "mount failed" 1>&2
+  exit 2
+fi
+
+exit /b 0
